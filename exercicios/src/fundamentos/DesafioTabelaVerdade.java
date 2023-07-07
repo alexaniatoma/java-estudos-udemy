dcb54e7af90cb7fe3fb55f44113f2097371861e2
alexaniatoma <alexaniatoma@hotmail.com>
2023-07-07 11:57:46 -0300

    desafio tabela verdade

---
+++ exercicios/src/fundamentos/DesafioTabelaVerdade.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class DesafioTabelaVerdade {
+	public static void main(String[] args) {
+
+		boolean trabalhoTerca = false;
+		boolean trabalhoQuinta = false;
+
+		if (trabalhoTerca && trabalhoQuinta) {
+			System.out.println("Comprar Tv 50pol. e tomar sorvete");
+		} else if (trabalhoTerca || trabalhoQuinta) {
+			System.out.println("Comprar TV 32pol. e tomar sorvete");
+		}else {
+			System.out.println("Sem TVs e sem Sorvete");
+		}
+	}
+}
