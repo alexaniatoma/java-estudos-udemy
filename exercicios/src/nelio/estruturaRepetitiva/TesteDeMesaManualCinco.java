e214b894cefe604f5afcad36edd8d09b9f1cd66e
alexaniatoma <alexaniatoma@hotmail.com>
2023-07-17 13:41:36 -0300

    teste de mesa cinco, seis e sete

---
+++ exercicios/src/nelio/estruturaRepetitiva/TesteDeMesaManualCinco.java
@@ -0,0 +1,16 @@
+package nelio.estruturaRepetitiva;
+
+public class TesteDeMesaManualCinco {
+
+	public static void main(String[] args) {
+		int x = 0;
+		
+		while(x < 5) {
+			int y = x * 3;
+			System.out.print(y);
+			x = x +1;
+		}
+		System.out.println("Fim");
+	}
+
+}
---
+++ exercicios/src/nelio/estruturaRepetitiva/TesteDeMesaManualSeis.java
@@ -0,0 +1,21 @@
+package nelio.estruturaRepetitiva;
+
+public class TesteDeMesaManualSeis {
+	public static void main(String[] args) {
+		
+		int x = 2;
+		int y = 10;
+		
+		System.out.println("Olá");
+		
+		while(x < y) {
+			System.out.println(x + "-" + y);
+			x = x * 2;
+			y = y + 1;
+			
+		}
+		
+		
+	}
+
+}
