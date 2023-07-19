14c3ef0505e0a7e0b1c8b130cb208265915577ed
alexaniatoma <alexaniatoma@hotmail.com>
2023-07-19 17:33:42 -0300

    exercicio simples for

---
+++ exercicios/src/nelio/estruturaRepetitiva/For.java
@@ -0,0 +1,12 @@
+package nelio.estruturaRepetitiva;
+
+public class For {
+	public static void main(String[] args) {
+		for(int i = 5; i>0; i --) {
+			System.out.println("Valor de i: " + i);
+		}
+		
+	}
+	
+
+}
