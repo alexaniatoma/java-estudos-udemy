58d4234924e08b7bc18f419655bd05f61d249547
alexaniatoma <alexaniatoma@hotmail.com>
2023-07-20 11:50:57 -0300

    teste for sem valor numerico

---
+++ exercicios/src/controle/DesafioFor.java
@@ -0,0 +1,24 @@
+package controle;
+
+public class DesafioFor {
+	
+	public static void main(String[] args) {
+		
+		String valor = "#";
+		
+		for(int i = 1 ; i<=5 ; i++) {
+			System.out.println(valor);
+			valor += valor;
+		}
+		
+		/*******************Desafio ***************
+		 * Não pode usar valor numérico para controlar o laço!
+		 */
+		
+		
+		for(String estrela = "*"; !estrela.equals("**********"); estrela +="*") {
+			System.out.println(estrela);
+		}
+	}
+
+}
